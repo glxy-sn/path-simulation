85a84768b6ff2d0fcd17e2cc5f33f8c4c08f77bd
Tiara <tiarashafa6@gmail.com>
2026-08-03 13:29:10 +0700

    Initial Commit

---
+++ foodcourt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  foodcourt
+//
+//  Created by Shafa Tiara on 03/08/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
